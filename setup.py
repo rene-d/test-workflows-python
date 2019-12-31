8126dbb88c285c33c747dbc517e35a484433e77a
René Devichi <rene.devichi@gmail.com>
2019-12-31 07:48:44 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import sys
 from setuptools import setup, find_packages
 from os import path
 
+here = path.abspath(path.dirname(__file__))
+
 
 # Get the long description from the README file
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
